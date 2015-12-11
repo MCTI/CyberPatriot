54d3a2c013d5c092df41102884f03e6e7f3dbeae
Cody Kaczynski <cxdy@users.noreply.github.com>
2015-12-10 21:00:35 -0500

    Secure.SH Version 2

--- harden.sh
+++ harden.sh
@@ -126,8 +126,64 @@ disable_root_account() {
 }
 
 update_kernel() {
-
+    echo "Updating kernel to v4.3.."
+    cd /tmp/
+    wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.3-wily/linux-headers-4.3.0-040300-generic_4.3.0-040300.201511020949_amd64.deb
+    wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.3-wily/linux-headers-4.3.0-040300_4.3.0-040300.201511020949_all.deb
+    wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.3-wily/linux-image-4.3.0-040300-generic_4.3.0-040300.201511020949_amd64.deb
+    dpkg -i *.deb
+    echo "Kernel updated! You WILL need to reboot."
 }
+
+install_software() {
+    cd /
+    echo "Installing firefox, hardinfo, chkrootkit, iptables, portsentry, lynis & ufw.."
+    apt-get -V -y install firefox, hardinfo, chkrootkit, iptables, portsentry, lynis, ufw
+    apt-get update
+    apt-get upgrade
+}
+
+log_antivirus() {
+    mkdir -v $HOME/.log-files
+    echo "Logging info from antivirus & security tools"
+    hardinfo -r -f html > /$HOME/Desktop/.logfiles/hardinfo-html.html
+    chkrootkit > /$HOME/Desktop/.logfiles/chkrootkit.log
+    checkps > /$HOME/Desktop/.logfiles/checkps.log
+    lynis -c > /$HOME/Desktop/.logfiles/lynis.log
+}
+
+firewall_config() {
+    echo "Firewall configuration.."
+    iptables -A INPUT -p tcp -s 0/0 -d 0/0 --dport 23 -j DROP         #Block Telnet
+    echo "Blocked Telnet!"
+    iptables -A INPUT -p tcp -s 0/0 -d 0/0 --dport 2049 -j DROP       #Block NFS
+    echo "Blocked NFS!"
+    iptables -A INPUT -p udp -s 0/0 -d 0/0 --dport 2049 -j DROP       #Block NFS
+    echo "Blocked NFS!"
+    iptables -A INPUT -p tcp -s 0/0 -d 0/0 --dport 6000:6009 -j DROP  #Block X-Windows
+    echo "Blocked X-Windows!"
+    iptables -A INPUT -p tcp -s 0/0 -d 0/0 --dport 7100 -j DROP       #Block X-Windows font server
+    echo "Blocked X-Windows font server!"
+    iptables -A INPUT -p tcp -s 0/0 -d 0/0 --dport 515 -j DROP        #Block printer port
+    echo "Blocked printer port!"
+    iptables -A INPUT -p udp -s 0/0 -d 0/0 --dport 515 -j DROP        #Block printer port
+    echo "Blocked printer port!"
+    iptables -A INPUT -p tcp -s 0/0 -d 0/0 --dport 111 -j DROP        #Block Sun rpc/NFS
+    echo "Blocked Sun RPC/NFS!"
+    iptables -A INPUT -p udp -s 0/0 -d 0/0 --dport 111 -j DROP        #Block Sun rpc/NFS
+    echo "Blocked Sun RPC/NFS!"
+    iptables -A INPUT -p all -s localhost  -i eth0 -j DROP            #Deny outside packets from internet which claim to be from your loopback interface.
+    echo "Blocked fake loopback packets!"
+}
+
+echo "##### Configuring firewall #####"
+firewall_config
+
+echo "##### Installing security tools #####"
+install_software
+
+echo "##### Logging info from security tools #####"
+log_antivirus
 
 echo "##### Setting apt update settings #####"
 set_update_settings
@@ -176,6 +232,14 @@ if [[ $answer == [yY] ]]; then
     disable_root_account
 else
     echo "Not locking root account."
+fi
+
+read -p "Do you want to update the kernel?" answer
+if [[ $answer == [yY] ]]; then
+    update_kernel
+else
+    echo "Kernel not updating"
+fi
 
 echo "##### Listing users in sensitive groups #####"
 list_sensitive_groups
