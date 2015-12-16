bf9ca09fe5840331f7b9f64d7cc242b349ab4d59
Cody Kaczynski <cxdy@users.noreply.github.com>
2015-12-16 12:58:35 -0500

    verification

--- harden.sh
+++ harden.sh
@@ -8,6 +8,21 @@ if [[ $EUID -ne 0 ]]; then
     exit 1
 fi
 
+# Checking if you read the scenario
+echo "Make sure you READ THE SCENARIO"
+echo "R E A D  T H E  S C E N A R I O"
+echo -n "Did you read scenario? (y/n) "
+old_stty_cfg=$(stty -g)
+stty raw -echo
+answer=$( while ! head -c 1 | grep -i '[ny]' ;do true ;done )
+stty $old_stty_cfg
+if echo "$answer" | grep -iq "^y" ;then
+    main
+else
+    echo "READ THE SCENARIO"
+    exit
+fi
+
 list_all_services() {
     service --status-all | less -P ":q to exit"
 }
