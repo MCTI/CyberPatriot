345dfac2ccddb7216cab43b5c87c34898260a50e
Cody Kaczynski <cxdy@users.noreply.github.com>
2015-12-16 12:59:05 -0500

    Update password.sh

--- password.sh
+++ password.sh
@@ -1,3 +1,9 @@
+# exit out if we're not root
+if [[ $EUID -ne 0 ]]; then
+    echo "This script must be run as root" 1>&2
+    exit 1
+fi
+
 # Checking if you read the scenario
 echo "Make sure you READ THE SCENARIO"
 echo "R E A D  T H E  S C E N A R I O"
