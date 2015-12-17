b4367e3f2c51618dea6d36399a7c5822af315c85
Cody Kaczynski <cxdy@users.noreply.github.com>
2015-12-17 09:09:30 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,16 +4,16 @@ if [[ $EUID -ne 0 ]]; then
     exit 1
 fi
 
-if [ -d "/scripts" ] then
+if [ -d "/scripts" ]; then
     echo "# Ubuntu Security Essentials"
     echo "# Welcome back"
     cd scripts
-    read -p "# Do you want to run the main CyberPatriot security script?" answer
+    read -p "# Do you want to run the main CyberPatriot security script? [Y/n]" answer
     if [[ $answer == [yY] ]]; then
         bash CyberPatriot.sh
     else
         echo "# Okay!"
-        read -p "Do you want to run the password policy script? (SHOULD BE RUN AT THE END OF THE COMPETITION IN DESPERATION" answer
+        read -p "Do you want to run the password policy script? (SHOULD BE RUN AT THE END OF THE COMPETITION IN DESPERATION) [Y/n]" answer
             if [[ $answer == [yY] ]]; then
                 bash password.sh
             else
@@ -26,8 +26,8 @@ else
     mkdir scripts
     cd scripts
     echo "# Downloading scripts.."
-    wget https://gist.githubusercontent.com/cxdy/47a31af2c57886eee678/raw/3223ebe19ce96b71d0ef78d1b74145ec70441148/CyberPatriot.sh
-    wget https://gist.githubusercontent.com/cxdy/cd62d4a93a905d299ef9/raw/f6e26335ca9f4c73661ced0e6ef8ba3f8db858b3/password.sh
+    wget https://gist.github.com/cxdy/47a31af2c57886eee678/raw/e6db3a018acecb60cb05669d1354dd710ff2af01/CyberPatriot.sh
+    wget https://gist.github.com/cxdy/cd62d4a93a905d299ef9/raw/c0f1ad7a0d5ae3c23c0f0e4eece90f31c1468dbf/password.sh
     echo "# Applying necessary permissions to scripts.."
     chmod +x CyberPatriot.sh
     chmod +x password.sh
