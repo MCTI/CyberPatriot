3f3ff650f743e3779aaa0fda6ac26fae723b8a07
Cody Kaczynski <cxdy@users.noreply.github.com>
2016-01-12 09:17:26 -0500

    major update (fixes #3)

--- run.sh
+++ run.sh
@@ -18,17 +18,50 @@ if echo "$answer" | grep -iq "^y" ;then
       echo "# Ubuntu Security Essentials"
       echo "# Welcome back"
       cd scripts
-      read -p "# Do you want to run the main CyberPatriot security script? [Y/n]" answer
-      if [[ $answer == [yY] ]]; then
-          bash CyberPatriot.sh
+      echo "# Choose a script.."
+      echo ""
+      echo ""
+      echo "# 1. CyberPatriot.SH (Main script, provides security essentials and may give some points)"
+      echo "# 2. Password.SH (Password policy script, run near the end of the competition for extra points)"
+      echo "# 3. Portscan.SH (Scans the open ports and performs a SYN scan)"
+      echo "# 4. Exit (Exits Run.SH)"
+      echo ""
+      echo "# Warning: Only respond with 1, 2, 3 or 4. Responding with anything other than 1, 2, 3 or 4 will cause an error"
+      echo "# Script output will automatically be logged to text files that correspond with the scripts."
+      read INPUT
+
+      if [ $INPUT -eq 1 ]; then
+        echo "# Running CyberPatriot.SH.."
+        echo "# Don't leave your system, user input is required."
+        sleep 3
+        bash CyberPatriot.sh > cyberpatriot.txt
+
+      elif [ $INPUT -eq 2 ]; then
+        echo "# Are you sure you want to run password.sh?"
+        echo "# Reminder: This should only be run towards the end of the competition due to possibility of being locked out of your admin account"
+        read -p "# Respond with Y/N" password
+        if [[ $password == [yY] ]]; then
+          sleep 3
+          bash password.sh > password.txt
+          else
+              echo "Good call."
+              echo "Exiting Run.SH now.."
+              echo "Re-run if necessary."
+              exit
+          fi
+
+      elif [ $INPUT -eq 3 ]; then
+        echo "# Running Portscan.SH"
+        sleep 3
+        bash portscan.sh > portscan.txt
+
+      elif [ $INPUT -eq 4 ]; then
+        echo "# Exiting script.."
+        echo "# Re-run as necessary"
+        exit
       else
-          echo "# Okay!"
-          read -p "Do you want to run the password policy script? (SHOULD BE RUN AT THE END OF THE COMPETITION IN DESPERATION) [Y/n]" answer
-              if [[ $answer == [yY] ]]; then
-                  bash password.sh
-              else
-          echo "Okay! Can always be run again."
-              fi
+        echo "# You suck at following directions."
+        exit
       fi
   else
       echo "# Ubuntu Security Essentials"
@@ -38,22 +71,62 @@ if echo "$answer" | grep -iq "^y" ;then
       echo "# Downloading scripts.."
       wget https://gist.github.com/cxdy/47a31af2c57886eee678/raw/e6db3a018acecb60cb05669d1354dd710ff2af01/CyberPatriot.sh
       wget https://gist.github.com/cxdy/cd62d4a93a905d299ef9/raw/f10e6a421dc1ffd16942085cfe87cda3ee94ccae/password.sh
+      wget https://gist.githubusercontent.com/cxdy/bfd4359d1a747fcbe1f1/raw/00ce91d87ef9fb09dbe973a91b3c8f03095bea23/portscan.sh
       echo "# Applying necessary permissions to scripts.."
       chmod +x CyberPatriot.sh
       chmod +x password.sh
+      chmod +x portscan.sh
       echo "# All set!"
-      read -p "# Do you want to run the main CyberPatriot security script?" answer
-          if [[ $answer == [yY] ]]; then
-              bash CyberPatriot.sh
+      echo ""
+      echo ""
+      echo "# RUNNING MAIN SCRIPT #"
+      echo ""
+      echo ""
+      echo "# Choose a script.."
+      echo ""
+      echo ""
+      echo "# 1. CyberPatriot.SH (Main script, provides security essentials and may give some points)"
+      echo "# 2. Password.SH (Password policy script, run near the end of the competition for extra points)"
+      echo "# 3. Portscan.SH (Scans the open ports and performs a SYN scan)"
+      echo "# 4. Exit (Exits Run.SH)"
+      echo ""
+      echo "# Warning: Only respond with 1, 2, 3 or 4. Responding with anything other than 1, 2, 3 or 4 will cause an error"
+      echo "# Script output will automatically be logged to text files that correspond with the scripts."
+      read INPUT
+
+      if [ $INPUT -eq 1 ]; then
+        echo "# Running CyberPatriot.SH.."
+        echo "# Don't leave your system, user input is required."
+        sleep 3
+        bash CyberPatriot.sh > cyberpatriot.txt
+
+      elif [ $INPUT -eq 2 ]; then
+        echo "# Are you sure you want to run password.sh?"
+        echo "# Reminder: This should only be run towards the end of the competition due to possibility of being locked out of your admin account"
+        read -p "# Respond with Y/N" password
+        if [[ $password == [yY] ]]; then
+          sleep 3
+          bash password.sh > password.txt
           else
-              echo "# Okay!"
-              read -p "Do you want to run the password policy script? (SHOULD BE RUN AT THE END OF THE COMPETITION IN DESPERATION" answer
-                  if [[ $answer == [yY] ]]; then
-                      bash password.sh
-                  else
-                      echo "Okay! Can always be run again."
-                  fi
+              echo "Good call."
+              echo "Exiting Run.SH now.."
+              echo "Re-run if necessary."
+              exit
           fi
+
+      elif [ $INPUT -eq 3 ]; then
+        echo "# Running Portscan.SH"
+        sleep 3
+        bash portscan.sh > portscan.txt
+
+      elif [ $INPUT -eq 4 ]; then
+        echo "# Exiting script.."
+        echo "# Re-run as necessary"
+        exit
+      else
+        echo "# You suck at following directions."
+        exit
+      fi
   # End of script
   fi
 
